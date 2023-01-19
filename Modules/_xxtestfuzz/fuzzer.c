66d6bd2bdeab39f6201a601d30a1799dd7b5acfb
Stanislav Osipov <57736012+stasos24@users.noreply.github.com>
2023-01-19 20:13:43 +0300

    Update fuzzer.c

--- Modules/_xxtestfuzz/fuzzer.c
+++ Modules/_xxtestfuzz/fuzzer.c
@@ -14,6 +14,57 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
+const char* encoding[101] = {"ascii","big5","big5hkscs","charmap","cp037","cp1006","cp1026","cp1125","cp1140","cp1250",
+                             "cp1251","cp1252","cp1253","cp1254","cp1255","cp1256","cp1257","cp1258","cp424","cp437",
+                             "cp500","cp720","cp737","cp775","cp850","cp852","cp855","cp856","cp857","cp858","cp860",
+                             "cp861","cp862","cp863","cp864","cp865","cp866","cp869","cp874","cp875","cp932","cp949",
+                             "cp950","euc_jis_2004","euc_jisx0213","euc_jp","euc_kr","gb18030","gb2312","gbk","hp_roman8",
+                             "hz","idna","iso2022_jp","iso2022_jp_1","iso2022_jp_2","iso2022_jp_2004","iso2022_jp_3",
+                             "iso2022_jp_ext","iso2022_kr","iso8859_1","iso8859_10","iso8859_11","iso8859_13","iso8859_14",
+                             "iso8859_15","iso8859_16","iso8859_2","iso8859_3","iso8859_4","iso8859_5","iso8859_6",
+                             "iso8859_7","iso8859_8","iso8859_9","johab","koi8_r","koi8_t","koi8_u","kz1048","latin_1",
+                             "mac_cyrillic","mac_greek","mac_iceland","mac_latin2","mac_roman","mac_turkish","palmos",
+                             "ptcp154","punycode","raw_unicode_escape","shift_jis","shift_jis_2004","shift_jisx0213",
+                             "tis_620","unicode_escape","utf_16","utf_16_be","utf_16_le","utf_7","utf_8"};
+//Iniatilisation xml.parser.expat.parse
+PyObject* xml_module = NULL;
+PyObject* xml_object = NULL;
+/* Called by LLVMFuzzerTestOneInput for initialization */
+static int init_xml_parse(void) {
+    /* Import expat.parsecreate */
+    PyObject* xml_module = PyImport_ImportModule("xml.parsers.expat");
+    if (xml_module == NULL) {
+        return 0;
+    }
+    xml_object = PyObject_GetAttrString(xml_module, "ParserCreate");
+    if (xml_object == NULL) {
+        return 0;
+    }
+    return xml_object != NULL;
+}
+/* Fuzz expat.parse(x) */
+static int fuzz_xml_parse(const char* data, size_t size) {
+    if (size < 2) {
+        return 0;
+    }
+    int index = abs(data[0]%101);
+    PyObject *argList = Py_BuildValue("sz", encoding[index], NULL);
+    /* Call the class object. */
+    PyObject *obj = PyObject_CallObject(xml_object, argList);
+    PyObject* input_bytes = PyBytes_FromStringAndSize(data, size);
+    if (input_bytes == NULL) {
+        return 0;
+    }
+    PyObject* parsed = PyObject_CallOneArg(obj, input_bytes);
+    if (parsed == NULL) {
+            PyErr_Clear();
+    }
+    Py_DECREF(obj);
+    Py_DECREF(argList);
+    Py_DECREF(input_bytes);
+    Py_XDECREF(parsed);
+    return 0;
+}
 
 PyObject* xml_loads_method = NULL;
 /* Called by LLVMFuzzerTestOneInput for initialization */
@@ -381,7 +432,7 @@ static int fuzz_sre_match(const char* data, size_t size) {
 //Initialization codec.encode
 PyObject* codecs_module=NULL;
 PyObject* codecs_method=NULL;
-const char* encoding[101] = {"ascii","big5","big5hkscs","charmap","cp037","cp1006","cp1026","cp1125","cp1140","cp1250","cp1251","cp1252","cp1253","cp1254","cp1255","cp1256","cp1257","cp1258","cp424","cp437","cp500","cp720","cp737","cp775","cp850","cp852","cp855","cp856","cp857","cp858","cp860","cp861","cp862","cp863","cp864","cp865","cp866","cp869","cp874","cp875","cp932","cp949","cp950","euc_jis_2004","euc_jisx0213","euc_jp","euc_kr","gb18030","gb2312","gbk","hp_roman8","hz","idna","iso2022_jp","iso2022_jp_1","iso2022_jp_2","iso2022_jp_2004","iso2022_jp_3","iso2022_jp_ext","iso2022_kr","iso8859_1","iso8859_10","iso8859_11","iso8859_13","iso8859_14","iso8859_15","iso8859_16","iso8859_2","iso8859_3","iso8859_4","iso8859_5","iso8859_6","iso8859_7","iso8859_8","iso8859_9","johab","koi8_r","koi8_t","koi8_u","kz1048","latin_1","mac_cyrillic","mac_greek","mac_iceland","mac_latin2","mac_roman","mac_turkish","palmos","ptcp154","punycode","raw_unicode_escape","shift_jis","shift_jis_2004","shift_jisx0213","tis_620","unicode_escape","utf_16","utf_16_be","utf_16_le","utf_7","utf_8"};
+
 static int init_codecs_encode(void) {
    codecs_module=PyImport_ImportModule("codecs");
    if (codecs_module == NULL) {
@@ -623,6 +674,16 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     }
 
     rv |= _run_fuzz(data, size, fuzz_codecs_encode);
+#endif
+#if !defined(_Py_FUZZ_ONE) || defined(_Py_FUZZ_fuzz_xml_parse)
+    static int XML_INITIALIZED = 0;
+    if (!XML_INITIALIZED && !init_xml_parse()) {
+        PyErr_Print();
+        abort();
+    } else {
+        XML_INITIALIZED = 1;
+    }
+    rv |= _run_fuzz(data, size, fuzz_xml_parse);
 #endif
   return rv;
 }
